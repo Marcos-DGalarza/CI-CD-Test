1f324b57efb729a641ce3da6d429f62565d4af04
Marcos-DGalarza <marcos.g3464@gmail.com>
2025-05-24 10:22:41 -0300

    agregando funcion de dividir

--- test_app.py
+++ test_app.py
@@ -4,13 +4,20 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
 
 def test_suma():
     assert suma(2, 3) == 5
     assert suma(-1, 1) == 0
     assert suma(0.3, 0.7) == 1
 
-
+def test_dividir():
+    assert dividir(10, 2) == 5
+    assert dividir(9, 3) == 3
+    
 def test_resta():
     assert resta(5, 2) == 3
     assert resta(1, 1) == 0
